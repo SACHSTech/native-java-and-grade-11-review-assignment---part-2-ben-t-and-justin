fdffb60a4827678e5e06680eede5f6ac022101ba
Benjamin Teh <>
2021-05-09 18:26:34 +0000

    FileIO2 V1

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -2,36 +2,14 @@ package gr11review.part2;
 import java.io.*;
 
 
+
 public class Main{
   public static void main(String[] args) throws IOException{
     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-    int count;
-    int sequenceCount = 0;
-    int letterCount = 0;
-    char letterHolder;
-    String sequenceHolder = "";
-    int finalSum = 0;
+    
     String str = keyboard.readLine();
 
-    for(count = 0; count < str.length(); count++){
-      letterHolder = str.charAt(count);
-      if(Character.isDigit(letterHolder)){
-        sequenceCount++;
-        sequenceHolder = sequenceHolder + letterHolder;
-        if(count == (str.length() - 1)){
-          finalSum = finalSum + Integer.parseInt(sequenceHolder);
-        }
-      }
-      else{
-        letterCount++;
-        if(sequenceCount > 0){
-        finalSum = finalSum + Integer.parseInt(sequenceHolder);
-        sequenceHolder = "";
-        sequenceCount = 0;
-        }
-      }
-    }
-    System.out.println(finalSum);
+    System.out.println(Utility.sumNumbers(str));
     
   }
 }
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,10 +1,52 @@
 package gr11review.part2;
 import java.io.*;
+import java.util.*;
 
 public class Utility {
 
+/**
+* Scan each letter and check if it is a number.
+* If it is a number, add it to a temporary string holder.
+* Once a non-number is reached, convert the string into a number and add it to the total, and wipe the string. Use a counter to trigger this.
+* Make sure that the sum will add even if the last character of the string is a number.
+@author Benjamin Teh
+*/
   public static int sumNumbers(String str){
-    System.out.println(Character.isDigit());
+    int count;
+    int sequenceCount = 0;
+    char letterHolder;
+    String sequenceHolder = "";
+    int finalSum = 0;
+
+    for (count = 0; count < str.length(); count++){
+      letterHolder = str.charAt(count);
+      if (Character.isDigit(letterHolder)){
+        sequenceCount++;
+        sequenceHolder = sequenceHolder + letterHolder;
+        if (count == (str.length() - 1)){
+          finalSum = finalSum + Integer.parseInt(sequenceHolder);
+        }
+      }
+      else {
+        if (sequenceCount > 0){
+        finalSum = finalSum + Integer.parseInt(sequenceHolder);
+        sequenceHolder = "";
+        sequenceCount = 0;
+        }
+      }
+    }
+    return finalSum;
+  }
+
+/**
+* In words.txt there are two words that start with "a".
+* Therefore we must run a loop checking the first two letters of each word. Each word that is higher in the order than the one before will be replacing the previous word until the file is done running.
+*/
+  public static String alphaWord(String filenametxt){
+    BufferedReader file = new BufferedReader(new FileReader(filenametxt));
+
+    String firstWord;
+
   }
     
 
