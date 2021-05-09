81484560a828fb5faaab5c764a83ca6280a05042
Justin Ho <>
2021-05-09 00:50:00 +0000

    Save 1

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,15 @@
 package gr11review.part2;
 
+import java.io.*;
+import gr11review.part2.Utility.*;
+
+public class Main { 
+  public static void main(String[] args) throws IOException {
+    BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+    Utility utility = new Utility();
+    System.out.println(utility.zipZap("zipXzap"));
+
+  }
+ 
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -2,8 +2,22 @@ package gr11review.part2;
 import java.io.*;
 
 public class Utility {
+   public static String zipZap(String str) {
 
-    
+     int strlength = str.length();
+     String newString = "";
 
-    
-}
+     for (int count = 0; count < strlength - 1; count++) {
+
+       if(str.substring(count,count+ 1).equalsIgnoreCase("z") && str.substring(count + 2,count + 3).equalsIgnoreCase("p")){
+         newString += str.substring(count,count+ 1);
+         newString += str.substring(count+ 2,count+ 3);
+         count += 2;
+       }else{
+         newString += str.substring(count,count+ 1); 
+       } 
+     }
+     return newString; 
+   }
+  }
+  
