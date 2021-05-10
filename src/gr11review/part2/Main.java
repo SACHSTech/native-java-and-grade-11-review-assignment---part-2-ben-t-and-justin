cad9d4143308042be2b233b2c0f3f09f855f59e5
Benjamin Teh <>
2021-05-10 02:59:40 +0000

    Arrays6 V3

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,13 +1,8 @@
 package gr11review.part2;
 import java.io.*;
-import java.util.Arrays;
-
-
 
 public class Main{
   public static void main(String[] args) throws IOException{
-    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(keyboard.readLine());
     
   }
 }
